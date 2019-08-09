a6f81704548e21341c9343d20bd43262660b1631
munificent-commit-7390 <jaredjames2020@gmail.com>
2019-08-09 23:55:04 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,6 +6,8 @@
 def introduction(name)
   "Hi, my name is #{name}."
 end
+introduction("Josh")
+
 
 def introduction_with_language(name, language)
   "Hi, my name is #{name} and I am learning to program in #{language}."
